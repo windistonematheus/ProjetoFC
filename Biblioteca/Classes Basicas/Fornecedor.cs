d369290365fd3d59d33a92b423d0b82df9e212e1
DiegoMaylan <diego_maylan@hotmail.com>
2017-10-13 09:29:01 -0300

    Add project files.

---
+++ Biblioteca/Classes Basicas/Fornecedor.cs
@@ -0,0 +1,172 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Classes_Basicas
+{
+    public class Fornecedor
+    {
+        //Declaração dos atributos
+        private int id;
+        private string cnpj;
+        private string cep;
+        private string razaoSocail;
+        private string logradouro;
+        private string cidade;
+        private string estado;
+        private string bairro;
+        private string email;
+        private string telefone;
+        private List<Compra> compra;
+
+        public Fornecedor()
+        {
+            List<Compra> compra = new List<Compra>();
+        }
+        //Métodos Gets e Sets
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Cnpj
+        {
+            get
+            {
+                return cnpj;
+            }
+
+            set
+            {
+                cnpj = value;
+            }
+        }
+
+        public string Cep
+        {
+            get
+            {
+                return cep;
+            }
+
+            set
+            {
+                cep = value;
+            }
+        }
+
+        public string RazaoSocail
+        {
+            get
+            {
+                return razaoSocail;
+            }
+
+            set
+            {
+                razaoSocail = value;
+            }
+        }
+
+        public string Logradouro
+        {
+            get
+            {
+                return logradouro;
+            }
+
+            set
+            {
+                logradouro = value;
+            }
+        }
+
+        public string Cidade
+        {
+            get
+            {
+                return cidade;
+            }
+
+            set
+            {
+                cidade = value;
+            }
+        }
+
+        public string Estado
+        {
+            get
+            {
+                return estado;
+            }
+
+            set
+            {
+                estado = value;
+            }
+        }
+
+        public string Bairro
+        {
+            get
+            {
+                return bairro;
+            }
+
+            set
+            {
+                bairro = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+
+            set
+            {
+                telefone = value;
+            }
+        }
+
+        public List<Compra> Compra
+        {
+            get
+            {
+                return compra;
+            }
+
+            set
+            {
+                compra = value;
+            }
+        }
+    }
+}
---
+++ Biblioteca/Classes Basicas/MateriaPrima.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Classes_Basicas
+{
+    public class MateriaPrima
+    {
+        //Declaração dos atributos
+        private int id;
+        private DateTime validade;
+        private string descricao;
+        private string lote;
+        private string nome;
+        private int estoqueAtual;
+        private List<Produto> produto;
+        private List<Compra> compra;
+
+        public MateriaPrima()
+        {
+            List<Produto> produto = new List<Produto>();
+            List<Compra> compra = new List<Compra>();
+        }
+
+        //Métodos Gets e Sets
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public DateTime Validade
+        {
+            get
+            {
+                return validade;
+            }
+
+            set
+            {
+                validade = value;
+            }
+        }
+
+        public string Descricao
+        {
+            get
+            {
+                return descricao;
+            }
+
+            set
+            {
+                descricao = value;
+            }
+        }
+
+        public string Lote
+        {
+            get
+            {
+                return lote;
+            }
+
+            set
+            {
+                lote = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public int EstoqueAtual
+        {
+            get
+            {
+                return estoqueAtual;
+            }
+
+            set
+            {
+                estoqueAtual = value;
+            }
+        }
+
+        public List<Produto> Produto
+        {
+            get
+            {
+                return produto;
+            }
+
+            set
+            {
+                produto = value;
+            }
+        }
+
+        internal List<Compra> Compra
+        {
+            get
+            {
+                return compra;
+            }
+
+            set
+            {
+                compra = value;
+            }
+        }
+
+        public Produto Produto1
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
