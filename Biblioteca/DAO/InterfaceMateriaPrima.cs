4ea3466047c7bf8761a8bb0cb99daafd598b04ab
Edson <edsonbarbosa@hotmail.com.br>
2017-10-31 20:46:21 -0300

    dao e interface do materia prima

---
+++ Biblioteca/DAO/InterfaceMateriaPrima.cs
@@ -0,0 +1,18 @@
+﻿using Biblioteca.Classes_Basicas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.DAO
+{
+    public interface InterfaceMateriaPrima
+    {
+        void Insert(MateriaPrima materiaprima);
+        void Update(MateriaPrima materiaprima);
+        void Delete(MateriaPrima materiaprima);
+        bool VerificarDuplicidade(MateriaPrima materiaprima);
+        List<MateriaPrima> Select(MateriaPrima filtro);
+    }
+}
