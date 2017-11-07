e33fefb8dcd98f3964f53c3b70ae32744e98777c
DiegoMaylan <diego_maylan@hotmail.com>
2017-11-07 19:10:38 -0300

    Revert "Atualizar form produto"
    This reverts commit c2b7febe30db68b8807f3a85ad614ec2035afb13.

--- ProjetoFabricaCosmeticos/FormCadastroProduto.Designer.cs
+++ ProjetoFabricaCosmeticos/FormCadastroProduto.Designer.cs
@@ -35,12 +35,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.buttonCadastrar = new System.Windows.Forms.Button();
-            this.comboBoxMateria = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.buttonAdicionarMateria = new System.Windows.Forms.Button();
-            this.listViewMateria = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.buttonRemoverMateria = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxNome
@@ -93,7 +87,7 @@
             // 
             // buttonCadastrar
             // 
-            this.buttonCadastrar.Location = new System.Drawing.Point(224, 315);
+            this.buttonCadastrar.Location = new System.Drawing.Point(216, 123);
             this.buttonCadastrar.Name = "buttonCadastrar";
             this.buttonCadastrar.Size = new System.Drawing.Size(75, 23);
             this.buttonCadastrar.TabIndex = 7;
@@ -101,69 +95,11 @@
             this.buttonCadastrar.UseVisualStyleBackColor = true;
             this.buttonCadastrar.Click += new System.EventHandler(this.buttonCadastrar_Click);
             // 
-            // comboBoxMateria
-            // 
-            this.comboBoxMateria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxMateria.FormattingEnabled = true;
-            this.comboBoxMateria.Location = new System.Drawing.Point(150, 115);
-            this.comboBoxMateria.Name = "comboBoxMateria";
-            this.comboBoxMateria.Size = new System.Drawing.Size(266, 21);
-            this.comboBoxMateria.TabIndex = 8;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(18, 118);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(74, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Matéria Prima:";
-            // 
-            // buttonAdicionarMateria
-            // 
-            this.buttonAdicionarMateria.Location = new System.Drawing.Point(422, 113);
-            this.buttonAdicionarMateria.Name = "buttonAdicionarMateria";
-            this.buttonAdicionarMateria.Size = new System.Drawing.Size(61, 23);
-            this.buttonAdicionarMateria.TabIndex = 10;
-            this.buttonAdicionarMateria.Text = "Add";
-            this.buttonAdicionarMateria.UseVisualStyleBackColor = true;
-            // 
-            // listViewMateria
-            // 
-            this.listViewMateria.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1});
-            this.listViewMateria.FullRowSelect = true;
-            this.listViewMateria.Location = new System.Drawing.Point(150, 155);
-            this.listViewMateria.Name = "listViewMateria";
-            this.listViewMateria.Size = new System.Drawing.Size(333, 97);
-            this.listViewMateria.TabIndex = 11;
-            this.listViewMateria.UseCompatibleStateImageBehavior = false;
-            this.listViewMateria.View = System.Windows.Forms.View.Details;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Matéria Prima";
-            this.columnHeader1.Width = 289;
-            // 
-            // buttonRemoverMateria
-            // 
-            this.buttonRemoverMateria.Location = new System.Drawing.Point(150, 258);
-            this.buttonRemoverMateria.Name = "buttonRemoverMateria";
-            this.buttonRemoverMateria.Size = new System.Drawing.Size(75, 23);
-            this.buttonRemoverMateria.TabIndex = 12;
-            this.buttonRemoverMateria.Text = "Remover";
-            this.buttonRemoverMateria.UseVisualStyleBackColor = true;
-            // 
             // FormCadastroProduto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(530, 350);
-            this.Controls.Add(this.buttonRemoverMateria);
-            this.Controls.Add(this.listViewMateria);
-            this.Controls.Add(this.buttonAdicionarMateria);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.comboBoxMateria);
+            this.ClientSize = new System.Drawing.Size(530, 158);
             this.Controls.Add(this.buttonCadastrar);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -188,11 +124,5 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button buttonCadastrar;
-        private System.Windows.Forms.ComboBox comboBoxMateria;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button buttonAdicionarMateria;
-        private System.Windows.Forms.ListView listViewMateria;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.Button buttonRemoverMateria;
     }
 }
